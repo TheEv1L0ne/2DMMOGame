9621754aa2cbc35a479720f7d09811d69c9e19f4
The Ev1L 0ne <marko.leskovsek.91@gmail.com>
2019-04-15 17:16:02 +0200

    Crated class for storing json data, Created script for getting and prasing JSON data into json data class

---
+++ 2DMMOGame/Assets/Scripts/JSONParser/JSONDataClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+[Serializable]
+public class JSONDataClass
+{
+    public int map_width;
+    public int map_height;
+    public int number_of_houses;
+    public Tiles[] tiles; 
+}
+
+[Serializable]
+public class Tiles
+{
+    public string type;
+    public string name;
+    public string level;
+}

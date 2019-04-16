cabd3a1c1879529bf21708deb68d4501bdfd1ecd
The Ev1L 0ne <marko.leskovsek.91@gmail.com>
2019-04-16 17:23:39 +0200

    Made the map zoomable

--- 2DMMOGame/Assets/Scripts/CameraController.cs
+++ 2DMMOGame/Assets/Scripts/CameraController.cs
@@ -4,22 +4,10 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private Vector3 position;
-    private float width;
-    private float height;
 
-    // Start is called before the first frame update
-    void Awake()
-    {
-        width = (float)Screen.width;
-        height = (float)Screen.height;
-
-        Debug.Log("WIDTH: " + width);
-        Debug.Log("HEIGHT: " + height);
-
-        // Position used for the cube.
-        position = new Vector3(0.0f, 0.0f, 0.0f);
-    }
+    public float orthoZoomSpeed = 0.5f;
+    public float minOrthographicSize = 3f;
+    public float maxOrthographicSize = 7f;
 
     Vector3 startTouchPositionUC; //UC - Unity Coordinates
     Vector3 movingTouchPositionUC;
@@ -27,11 +15,13 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0)
+        //Scroll map
+        if (Input.touchCount == 1)
         {
+            //Store touch
             Touch touch = Input.GetTouch(0);
 
-
+            //On touch down
             if (touch.phase == TouchPhase.Began)
             {
 
@@ -40,6 +30,7 @@ public class CameraController : MonoBehaviour
 
             }
 
+            //On touch moved
             if (touch.phase == TouchPhase.Moved)
             {
                 //Coordinates of touch while moving
@@ -47,6 +38,41 @@ public class CameraController : MonoBehaviour
 
                 //Camera should move for same distance as touch moved from its first click     
                 transform.position = transform.position + (startTouchPositionUC - movingTouchPositionUC);
+                transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
+            }
+        }
+
+        if(Input.touchCount == 2)
+        {
+            // Store both touches.
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            // Find the position in the previous frame of each touch.
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            // Find the magnitude of the vector (the distance) between the touches in each frame.
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            // Find the difference in the distances between each frame.
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            // If the camera is orthographic...
+            if (Camera.main.orthographic)
+            {
+                // ... change the orthographic size based on the change in distance between the touches.
+                if(Camera.main.orthographicSize >= minOrthographicSize && Camera.main.orthographicSize <= maxOrthographicSize)
+                    Camera.main.orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
+
+                //Makes sure that camere size do not fall below boundries
+                //MIN
+                if (Camera.main.orthographicSize < minOrthographicSize)
+                    Camera.main.orthographicSize = minOrthographicSize;
+                //MAX
+                if (Camera.main.orthographicSize > maxOrthographicSize)
+                    Camera.main.orthographicSize = maxOrthographicSize;
             }
         }
     }

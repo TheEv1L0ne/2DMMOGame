1279a1442f6877bc217fbf9546bf8c9d5230bbe9
The Ev1L 0ne <marko.leskovsek.91@gmail.com>
2019-04-16 19:03:04 +0200

    Fix problem where camera was moving out of map limits

--- 2DMMOGame/Assets/Scripts/CameraController.cs
+++ 2DMMOGame/Assets/Scripts/CameraController.cs
@@ -12,6 +12,14 @@ public class CameraController : MonoBehaviour
     Vector3 startTouchPositionUC; //UC - Unity Coordinates
     Vector3 movingTouchPositionUC;
 
+    float areaSize = 9.5f; //this should be provided from JSON at some point
+
+    float mapBoundriesXPositive;
+    float mapBoundriesXNegative;
+
+    float mapBoundriesYPositive;
+    float mapBoundriesYNegative;
+
     // Update is called once per frame
     void Update()
     {
@@ -28,23 +36,45 @@ public class CameraController : MonoBehaviour
                 //This is position in unity coordinates when touch on mobile devices occures
                 startTouchPositionUC = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 0f));
 
+
+                //calculates max and min position of camera where camere can move in unity coordinates based on camera size
+                //cause camera size can change based on zoom in/out
+                mapBoundriesXPositive = areaSize - Camera.main.orthographicSize * Camera.main.aspect;
+                mapBoundriesXNegative = -areaSize + Camera.main.orthographicSize * Camera.main.aspect;
+
+                mapBoundriesYPositive = areaSize - Camera.main.orthographicSize;
+                mapBoundriesYNegative = -areaSize + Camera.main.orthographicSize;
             }
 
             //On touch moved
             if (touch.phase == TouchPhase.Moved)
             {
-                //Coordinates of touch while moving
+                //Gets canera boubds
+                float cameraBoundriesXPOsitive = transform.position.x + (Camera.main.orthographicSize * Camera.main.aspect);
+                float cameraBoundriesXNegative = transform.position.x - (Camera.main.orthographicSize * Camera.main.aspect);
+
+                //This is position of touch in unity coordinates
                 movingTouchPositionUC = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 0f));
 
+                
                 //Camera should move for same distance as touch moved from its first click     
-                transform.position = transform.position + (startTouchPositionUC - movingTouchPositionUC);
-                transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
+
+                //calculates new x & y position for camera bases on distance touch traveled
+                float x = transform.position.x + (startTouchPositionUC.x - movingTouchPositionUC.x);
+                float y = transform.position.y + (startTouchPositionUC.y - movingTouchPositionUC.y);
+
+                //Checks to see if camera coordinates are out of bounds and put them back to max or min bounds
+                x = Mathf.Clamp(x, mapBoundriesXNegative, mapBoundriesXPositive);
+                y = Mathf.Clamp(y, mapBoundriesYNegative, mapBoundriesYPositive);
+
+                //Finally set new camera position
+                transform.position = new Vector3(x, y, -10f);
             }
         }
 
         if(Input.touchCount == 2)
         {
-            // Store both touches.
+            //Store both touches.
             Touch touchZero = Input.GetTouch(0);
             Touch touchOne = Input.GetTouch(1);
 
@@ -62,17 +92,24 @@ public class CameraController : MonoBehaviour
             // If the camera is orthographic...
             if (Camera.main.orthographic)
             {
-                // ... change the orthographic size based on the change in distance between the touches.
-                if(Camera.main.orthographicSize >= minOrthographicSize && Camera.main.orthographicSize <= maxOrthographicSize)
-                    Camera.main.orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
+                //Camera size
+                Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize + deltaMagnitudeDiff * orthoZoomSpeed, minOrthographicSize, maxOrthographicSize);
 
-                //Makes sure that camere size do not fall below boundries
-                //MIN
-                if (Camera.main.orthographicSize < minOrthographicSize)
-                    Camera.main.orthographicSize = minOrthographicSize;
-                //MAX
-                if (Camera.main.orthographicSize > maxOrthographicSize)
-                    Camera.main.orthographicSize = maxOrthographicSize;
+                //Changes camera position if zoom in/out would move camera out of bounds
+                float x = transform.position.x;
+                float y = transform.position.y;
+
+                mapBoundriesXPositive = areaSize - Camera.main.orthographicSize * Camera.main.aspect;
+                mapBoundriesXNegative = -areaSize + Camera.main.orthographicSize * Camera.main.aspect;
+
+                mapBoundriesYPositive = areaSize - Camera.main.orthographicSize;
+                mapBoundriesYNegative = -areaSize + Camera.main.orthographicSize;
+
+                x = Mathf.Clamp(x, mapBoundriesXNegative, mapBoundriesXPositive);
+                y = Mathf.Clamp(y, mapBoundriesYNegative, mapBoundriesYPositive);
+
+                transform.position = new Vector3(x, y, -10f);
+
             }
         }
     }
